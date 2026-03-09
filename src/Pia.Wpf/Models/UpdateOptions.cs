d11ba10a0fc4ff0e4b62bbf76f17c186980683a6
Marco Altmann <m.altmann@outlook.com>
2026-03-09 12:43:04 +0100

    #1 Update GitHub URLs to point to Pia.Wpf public repo

--- src/Pia.Wpf/Models/UpdateOptions.cs
+++ src/Pia.Wpf/Models/UpdateOptions.cs
@@ -4,7 +4,7 @@ public class AutoUpdateOptions
 {
     public const string SectionName = "Update";
 
-    public string GitHubRepoUrl { get; set; } = "https://github.com/Pia-Ai-dev/Pia";
+    public string GitHubRepoUrl { get; set; } = "https://github.com/Pia-Ai-dev/Pia.Wpf";
     public string? AccessToken { get; set; }
     public bool Prerelease { get; set; }
 }
